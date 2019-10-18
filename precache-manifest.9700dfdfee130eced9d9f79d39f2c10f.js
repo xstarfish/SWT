f70d217affb9b769e0297d82f9963c065920e809
xstarfish <1060614445@qq.com>
2019-10-18 09:45:02 +0800

    Updates

---
+++ precache-manifest.9700dfdfee130eced9d9f79d39f2c10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f5146efdb70e20b3f3b1c222f2fb88",
+    "url": "/SWT/index.html"
+  },
+  {
+    "revision": "4d117cbad353c451a0b9",
+    "url": "/SWT/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "516fb65a70df7ef34345",
+    "url": "/SWT/static/js/2.c4a6e226.chunk.js"
+  },
+  {
+    "revision": "4d117cbad353c451a0b9",
+    "url": "/SWT/static/js/main.54b7ae13.chunk.js"
+  },
+  {
+    "revision": "be5e9a8b582ae6029243",
+    "url": "/SWT/static/js/runtime-main.70855e7a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/SWT/static/media/logo.25bf045c.svg"
+  }
+]);
